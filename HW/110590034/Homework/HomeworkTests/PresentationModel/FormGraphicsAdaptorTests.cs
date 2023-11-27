9e1afe144676b032d7b9caf39e287e0abef3f111
kyynk <zhan.kiliinn@gmail.com>
2023-11-28 02:38:32 +0800

    -> drawing state, model, pmodel -> dynamic -> right bottom

---
+++ HW/110590034/Homework/HomeworkTests/PresentationModel/FormGraphicsAdaptorTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using Moq;
+
+namespace Homework.PresentationModel.Tests
+{
+    [TestClass()]
+    public class FormGraphicsAdaptorTests
+    {
+        FormGraphicsAdaptor _formGraphicsAdaptor;
+        //Mock<Graphics> graphicsMock = new Mock<Graphics>();
+
+        // setup
+        [TestInitialize()]
+        public void Initialize()
+        {
+            //_formGraphicsAdaptor = new FormGraphicsAdaptor();
+        }
+
+        [TestMethod()]
+        public void FormGraphicsAdaptorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawLineTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawRectangleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawEllipseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawHintTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawHintCircleTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
