cb05af1cf1f216d6174f89f4372db2f2dca1a133
kyynk <zhan.kiliinn@gmail.com>
2023-12-09 17:31:33 +0800

    dynamic layout ❔❔❔

--- HW/110590034/Homework/Homework/Form1.Designer.cs
+++ HW/110590034/Homework/Homework/Form1.Designer.cs
@@ -35,22 +35,30 @@
             this._information = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this._shapeTypeComboBox = new System.Windows.Forms.ComboBox();
             this._createButton = new System.Windows.Forms.Button();
-            this._canvases = new System.Windows.Forms.DataGridView();
             this._canvas1 = new System.Windows.Forms.Button();
             this._menuStrip = new System.Windows.Forms.MenuStrip();
             this._description = new System.Windows.Forms.ToolStripMenuItem();
             this._about = new System.Windows.Forms.ToolStripMenuItem();
             this._toolStrip = new System.Windows.Forms.ToolStrip();
-            this._canvas = new Homework.View.DoubleBufferedPanel();
             this._lineButton = new Homework.View.ToolStripBindingButton();
             this._rectangleButton = new Homework.View.ToolStripBindingButton();
             this._ellipseButton = new Homework.View.ToolStripBindingButton();
             this._defaultCursorButton = new Homework.View.ToolStripBindingButton();
+            this._canvas = new Homework.View.DoubleBufferedPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this._groupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this._shapeData)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this._canvases)).BeginInit();
             this._menuStrip.SuspendLayout();
             this._toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // _groupBox
@@ -58,9 +66,11 @@
             this._groupBox.Controls.Add(this._shapeData);
             this._groupBox.Controls.Add(this._shapeTypeComboBox);
             this._groupBox.Controls.Add(this._createButton);
-            this._groupBox.Location = new System.Drawing.Point(807, 72);
+            this._groupBox.Location = new System.Drawing.Point(2, 5);
+            this._groupBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._groupBox.Name = "_groupBox";
-            this._groupBox.Size = new System.Drawing.Size(313, 521);
+            this._groupBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._groupBox.Size = new System.Drawing.Size(235, 417);
             this._groupBox.TabIndex = 0;
             this._groupBox.TabStop = false;
             this._groupBox.Text = "資料顯示";
@@ -75,13 +85,14 @@
             this._deleteButton,
             this._shapeType,
             this._information});
-            this._shapeData.Location = new System.Drawing.Point(6, 67);
+            this._shapeData.Location = new System.Drawing.Point(4, 54);
+            this._shapeData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._shapeData.Name = "_shapeData";
             this._shapeData.ReadOnly = true;
             this._shapeData.RowHeadersVisible = false;
             this._shapeData.RowHeadersWidth = 51;
             this._shapeData.RowTemplate.Height = 27;
-            this._shapeData.Size = new System.Drawing.Size(301, 448);
+            this._shapeData.Size = new System.Drawing.Size(226, 358);
             this._shapeData.TabIndex = 0;
             this._shapeData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClickDeleteButton);
             // 
@@ -128,36 +139,29 @@
             "矩形",
             "線",
             "圓"});
-            this._shapeTypeComboBox.Location = new System.Drawing.Point(101, 24);
+            this._shapeTypeComboBox.Location = new System.Drawing.Point(76, 19);
+            this._shapeTypeComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._shapeTypeComboBox.Name = "_shapeTypeComboBox";
-            this._shapeTypeComboBox.Size = new System.Drawing.Size(156, 23);
+            this._shapeTypeComboBox.Size = new System.Drawing.Size(118, 20);
             this._shapeTypeComboBox.TabIndex = 1;
             // 
             // _createButton
             // 
-            this._createButton.Location = new System.Drawing.Point(6, 24);
+            this._createButton.Location = new System.Drawing.Point(4, 19);
+            this._createButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._createButton.Name = "_createButton";
-            this._createButton.Size = new System.Drawing.Size(64, 37);
+            this._createButton.Size = new System.Drawing.Size(48, 30);
             this._createButton.TabIndex = 0;
             this._createButton.Text = "新增";
             this._createButton.UseVisualStyleBackColor = true;
             this._createButton.Click += new System.EventHandler(this.ClickCreateButton);
             // 
-            // _canvases
-            // 
-            this._canvases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this._canvases.Location = new System.Drawing.Point(3, 72);
-            this._canvases.Name = "_canvases";
-            this._canvases.RowHeadersWidth = 51;
-            this._canvases.RowTemplate.Height = 27;
-            this._canvases.Size = new System.Drawing.Size(141, 521);
-            this._canvases.TabIndex = 1;
-            // 
             // _canvas1
             // 
-            this._canvas1.Location = new System.Drawing.Point(12, 79);
+            this._canvas1.Location = new System.Drawing.Point(11, 8);
+            this._canvas1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._canvas1.Name = "_canvas1";
-            this._canvas1.Size = new System.Drawing.Size(121, 70);
+            this._canvas1.Size = new System.Drawing.Size(91, 56);
             this._canvas1.TabIndex = 2;
             this._canvas1.UseVisualStyleBackColor = true;
             // 
@@ -168,7 +172,8 @@
             this._description});
             this._menuStrip.Location = new System.Drawing.Point(0, 0);
             this._menuStrip.Name = "_menuStrip";
-            this._menuStrip.Size = new System.Drawing.Size(1132, 27);
+            this._menuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this._menuStrip.Size = new System.Drawing.Size(849, 24);
             this._menuStrip.TabIndex = 4;
             this._menuStrip.Text = "menuStrip1";
             // 
@@ -177,13 +182,13 @@
             this._description.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this._about});
             this._description.Name = "_description";
-            this._description.Size = new System.Drawing.Size(53, 26);
+            this._description.Size = new System.Drawing.Size(43, 20);
             this._description.Text = "說明";
             // 
             // _about
             // 
             this._about.Name = "_about";
-            this._about.Size = new System.Drawing.Size(122, 26);
+            this._about.Size = new System.Drawing.Size(98, 22);
             this._about.Text = "關於";
             // 
             // _toolStrip
@@ -194,18 +199,11 @@
             this._rectangleButton,
             this._ellipseButton,
             this._defaultCursorButton});
-            this._toolStrip.Location = new System.Drawing.Point(0, 27);
+            this._toolStrip.Location = new System.Drawing.Point(0, 24);
             this._toolStrip.Name = "_toolStrip";
-            this._toolStrip.Size = new System.Drawing.Size(1132, 27);
+            this._toolStrip.Size = new System.Drawing.Size(849, 27);
             this._toolStrip.TabIndex = 5;
             this._toolStrip.Text = "toolStrip1";
-            // 
-            // _canvas
-            // 
-            this._canvas.Location = new System.Drawing.Point(146, 72);
-            this._canvas.Name = "_canvas";
-            this._canvas.Size = new System.Drawing.Size(655, 520);
-            this._canvas.TabIndex = 0;
             // 
             // _lineButton
             // 
@@ -213,7 +211,7 @@
             this._lineButton.Image = global::Homework.Properties.Resources.Line;
             this._lineButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this._lineButton.Name = "_lineButton";
-            this._lineButton.Size = new System.Drawing.Size(29, 28);
+            this._lineButton.Size = new System.Drawing.Size(24, 24);
             this._lineButton.Text = "toolStripButton1";
             this._lineButton.ToolTipText = "_lineButton";
             this._lineButton.Click += new System.EventHandler(this.ClickLineButton);
@@ -224,7 +222,7 @@
             this._rectangleButton.Image = global::Homework.Properties.Resources.Rectangle;
             this._rectangleButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this._rectangleButton.Name = "_rectangleButton";
-            this._rectangleButton.Size = new System.Drawing.Size(29, 28);
+            this._rectangleButton.Size = new System.Drawing.Size(24, 24);
             this._rectangleButton.Text = "toolStripButton2";
             this._rectangleButton.ToolTipText = "_rectangleButton";
             this._rectangleButton.Click += new System.EventHandler(this.ClickRectangleButton);
@@ -235,7 +233,7 @@
             this._ellipseButton.Image = global::Homework.Properties.Resources.Ellipse;
             this._ellipseButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this._ellipseButton.Name = "_ellipseButton";
-            this._ellipseButton.Size = new System.Drawing.Size(29, 28);
+            this._ellipseButton.Size = new System.Drawing.Size(24, 24);
             this._ellipseButton.Text = "toolStripButton3";
             this._ellipseButton.ToolTipText = "_ellipseButton";
             this._ellipseButton.Click += new System.EventHandler(this.ClickEllipseButton);
@@ -246,32 +244,92 @@
             this._defaultCursorButton.Image = global::Homework.Properties.Resources.DefaultCursor;
             this._defaultCursorButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this._defaultCursorButton.Name = "_defaultCursorButton";
-            this._defaultCursorButton.Size = new System.Drawing.Size(29, 24);
+            this._defaultCursorButton.Size = new System.Drawing.Size(24, 24);
             this._defaultCursorButton.Text = "toolStripButton1";
             this._defaultCursorButton.ToolTipText = "_defaultCursorButton";
             this._defaultCursorButton.Click += new System.EventHandler(this.ClickDefaultCursorButton);
             // 
+            // _canvas
+            // 
+            this._canvas.BackColor = System.Drawing.Color.White;
+            this._canvas.Location = new System.Drawing.Point(13, 68);
+            this._canvas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._canvas.Name = "_canvas";
+            this._canvas.Size = new System.Drawing.Size(464, 261);
+            this._canvas.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 51);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Silver;
+            this.splitContainer1.Panel1.Controls.Add(this._canvas1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(849, 433);
+            this.splitContainer1.SplitterDistance = 113;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.LightGray;
+            this.splitContainer2.Panel1.Controls.Add(this._canvas);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.splitContainer2.Panel2.Controls.Add(this._groupBox);
+            this.splitContainer2.Size = new System.Drawing.Size(733, 433);
+            this.splitContainer2.SplitterDistance = 490;
+            this.splitContainer2.SplitterWidth = 3;
+            this.splitContainer2.TabIndex = 0;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 605);
-            this.Controls.Add(this._canvas);
+            this.ClientSize = new System.Drawing.Size(849, 484);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this._toolStrip);
-            this.Controls.Add(this._canvas1);
-            this.Controls.Add(this._canvases);
-            this.Controls.Add(this._groupBox);
             this.Controls.Add(this._menuStrip);
             this.MainMenuStrip = this._menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this._groupBox.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this._shapeData)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this._canvases)).EndInit();
             this._menuStrip.ResumeLayout(false);
             this._menuStrip.PerformLayout();
             this._toolStrip.ResumeLayout(false);
             this._toolStrip.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -283,7 +341,6 @@
         private System.Windows.Forms.ComboBox _shapeTypeComboBox;
         private System.Windows.Forms.Button _createButton;
         private System.Windows.Forms.DataGridView _shapeData;
-        private System.Windows.Forms.DataGridView _canvases;
         private System.Windows.Forms.Button _canvas1;
         private System.Windows.Forms.MenuStrip _menuStrip;
         private System.Windows.Forms.ToolStripMenuItem _description;
@@ -297,6 +354,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn _information;
         private Homework.View.DoubleBufferedPanel _canvas;
         private Homework.View.ToolStripBindingButton _defaultCursorButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
     }
 }
 
