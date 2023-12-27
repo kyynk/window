e605afb6454ca5e19dd84cc0c03bf9266f8fb4cd
kyynk <zhan.kiliinn@gmail.com>
2023-12-28 00:29:17 +0800

    add setting create shape size

--- HW/110590034/Homework/CreateShapeForm.Designer.cs
+++ HW/110590034/Homework/CreateShapeForm.Designer.cs
@@ -29,119 +29,123 @@ namespace Homework
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this._textBoxLeft = new System.Windows.Forms.TextBox();
-            this._textBoxTop = new System.Windows.Forms.TextBox();
-            this._textBoxRight = new System.Windows.Forms.TextBox();
-            this._textBoxBottom = new System.Windows.Forms.TextBox();
+            this._label1 = new System.Windows.Forms.Label();
+            this._label2 = new System.Windows.Forms.Label();
+            this._label3 = new System.Windows.Forms.Label();
+            this._label4 = new System.Windows.Forms.Label();
+            this._leftTextBox = new System.Windows.Forms.TextBox();
+            this._topTextBox = new System.Windows.Forms.TextBox();
+            this._rightTextBox = new System.Windows.Forms.TextBox();
+            this._bottomTextBox = new System.Windows.Forms.TextBox();
             this._okButton = new System.Windows.Forms.Button();
             this._cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // _label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(131, 104);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "左上角座標X";
+            this._label1.AutoSize = true;
+            this._label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._label1.Location = new System.Drawing.Point(47, 31);
+            this._label1.Name = "_label1";
+            this._label1.Size = new System.Drawing.Size(129, 21);
+            this._label1.TabIndex = 0;
+            this._label1.Text = "左上角座標X";
             // 
-            // label2
+            // _label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(517, 104);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 21);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "左上角座標Y";
+            this._label2.AutoSize = true;
+            this._label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._label2.Location = new System.Drawing.Point(218, 31);
+            this._label2.Name = "_label2";
+            this._label2.Size = new System.Drawing.Size(129, 21);
+            this._label2.TabIndex = 1;
+            this._label2.Text = "左上角座標Y";
             // 
-            // label3
+            // _label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(131, 235);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 21);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "右下角座標X";
+            this._label3.AutoSize = true;
+            this._label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._label3.Location = new System.Drawing.Point(47, 162);
+            this._label3.Name = "_label3";
+            this._label3.Size = new System.Drawing.Size(129, 21);
+            this._label3.TabIndex = 2;
+            this._label3.Text = "右下角座標X";
             // 
-            // label4
+            // _label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(517, 235);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(129, 21);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "右下角座標Y";
+            this._label4.AutoSize = true;
+            this._label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._label4.Location = new System.Drawing.Point(218, 162);
+            this._label4.Name = "_label4";
+            this._label4.Size = new System.Drawing.Size(129, 21);
+            this._label4.TabIndex = 3;
+            this._label4.Text = "右下角座標Y";
             // 
-            // _textBoxLeft
+            // _leftTextBox
             // 
-            this._textBoxLeft.Location = new System.Drawing.Point(135, 145);
-            this._textBoxLeft.Name = "_textBoxLeft";
-            this._textBoxLeft.Size = new System.Drawing.Size(125, 22);
-            this._textBoxLeft.TabIndex = 4;
+            this._leftTextBox.Location = new System.Drawing.Point(51, 69);
+            this._leftTextBox.Name = "_leftTextBox";
+            this._leftTextBox.Size = new System.Drawing.Size(125, 22);
+            this._leftTextBox.TabIndex = 4;
             // 
-            // _textBoxTop
+            // _topTextBox
             // 
-            this._textBoxTop.Location = new System.Drawing.Point(521, 145);
-            this._textBoxTop.Name = "_textBoxTop";
-            this._textBoxTop.Size = new System.Drawing.Size(125, 22);
-            this._textBoxTop.TabIndex = 5;
+            this._topTextBox.Location = new System.Drawing.Point(222, 69);
+            this._topTextBox.Name = "_topTextBox";
+            this._topTextBox.Size = new System.Drawing.Size(125, 22);
+            this._topTextBox.TabIndex = 5;
             // 
-            // _textBoxRight
+            // _rightTextBox
             // 
-            this._textBoxRight.Location = new System.Drawing.Point(135, 278);
-            this._textBoxRight.Name = "_textBoxRight";
-            this._textBoxRight.Size = new System.Drawing.Size(125, 22);
-            this._textBoxRight.TabIndex = 6;
+            this._rightTextBox.Location = new System.Drawing.Point(51, 204);
+            this._rightTextBox.Name = "_rightTextBox";
+            this._rightTextBox.Size = new System.Drawing.Size(125, 22);
+            this._rightTextBox.TabIndex = 6;
             // 
-            // _textBoxBottom
+            // _bottomTextBox
             // 
-            this._textBoxBottom.Location = new System.Drawing.Point(521, 278);
-            this._textBoxBottom.Name = "_textBoxBottom";
-            this._textBoxBottom.Size = new System.Drawing.Size(125, 22);
-            this._textBoxBottom.TabIndex = 7;
+            this._bottomTextBox.Location = new System.Drawing.Point(222, 204);
+            this._bottomTextBox.Name = "_bottomTextBox";
+            this._bottomTextBox.Size = new System.Drawing.Size(125, 22);
+            this._bottomTextBox.TabIndex = 7;
             // 
             // _okButton
             // 
-            this._okButton.Location = new System.Drawing.Point(259, 381);
+            this._okButton.Font = new System.Drawing.Font("新細明體", 11F);
+            this._okButton.Location = new System.Drawing.Point(78, 250);
             this._okButton.Name = "_okButton";
             this._okButton.Size = new System.Drawing.Size(75, 23);
             this._okButton.TabIndex = 8;
-            this._okButton.Text = "ok";
+            this._okButton.Text = "OK";
             this._okButton.UseVisualStyleBackColor = true;
+            this._okButton.Click += new System.EventHandler(this._okButton_Click);
             // 
             // _cancelButton
             // 
-            this._cancelButton.Location = new System.Drawing.Point(446, 381);
+            this._cancelButton.Font = new System.Drawing.Font("新細明體", 11F);
+            this._cancelButton.Location = new System.Drawing.Point(247, 250);
             this._cancelButton.Name = "_cancelButton";
             this._cancelButton.Size = new System.Drawing.Size(75, 23);
             this._cancelButton.TabIndex = 9;
-            this._cancelButton.Text = "cancel";
+            this._cancelButton.Text = "Cancel";
             this._cancelButton.UseVisualStyleBackColor = true;
+            this._cancelButton.Click += new System.EventHandler(this._cancelButton_Click);
             // 
             // CreateShapeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(403, 313);
             this.Controls.Add(this._cancelButton);
             this.Controls.Add(this._okButton);
-            this.Controls.Add(this._textBoxBottom);
-            this.Controls.Add(this._textBoxRight);
-            this.Controls.Add(this._textBoxTop);
-            this.Controls.Add(this._textBoxLeft);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this._bottomTextBox);
+            this.Controls.Add(this._rightTextBox);
+            this.Controls.Add(this._topTextBox);
+            this.Controls.Add(this._leftTextBox);
+            this.Controls.Add(this._label4);
+            this.Controls.Add(this._label3);
+            this.Controls.Add(this._label2);
+            this.Controls.Add(this._label1);
             this.Name = "CreateShapeForm";
             this.Text = "CreateShape";
             this.ResumeLayout(false);
@@ -151,14 +155,14 @@ namespace Homework
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox _textBoxLeft;
-        private System.Windows.Forms.TextBox _textBoxTop;
-        private System.Windows.Forms.TextBox _textBoxRight;
-        private System.Windows.Forms.TextBox _textBoxBottom;
+        private System.Windows.Forms.Label _label1;
+        private System.Windows.Forms.Label _label2;
+        private System.Windows.Forms.Label _label3;
+        private System.Windows.Forms.Label _label4;
+        private System.Windows.Forms.TextBox _leftTextBox;
+        private System.Windows.Forms.TextBox _topTextBox;
+        private System.Windows.Forms.TextBox _rightTextBox;
+        private System.Windows.Forms.TextBox _bottomTextBox;
         private System.Windows.Forms.Button _okButton;
         private System.Windows.Forms.Button _cancelButton;
     }
--- HW/110590034/Homework/CreateShapeForm.cs
+++ HW/110590034/Homework/CreateShapeForm.cs
@@ -12,9 +12,105 @@ namespace Homework
 {
     public partial class CreateShapeForm : Form
     {
+        private double _left;
+        private double _top;
+        private double _right;
+        private double _bottom;
+        private bool _isOk;
+        private Size _canvasSize;
+
         public CreateShapeForm()
         {
             InitializeComponent();
+            _left = -1;
+            _top = -1;
+            _right = -1;
+            _bottom = -1;
+            _isOk = false;
+            _canvasSize = new Size(Constant.Constant.DEFAULT_MAX_PANEL_X, Constant.Constant.DEFAULT_MAX_PANEL_Y);
+
+            _leftTextBox.TextChanged += HandleTextBoxTextChanged;
+            _topTextBox.TextChanged += HandleTextBoxTextChanged;
+            _rightTextBox.TextChanged += HandleTextBoxTextChanged;
+            _bottomTextBox.TextChanged += HandleTextBoxTextChanged;
+            _okButton.Enabled = false;
+        }
+
+        public bool IsOk
+        {
+            get
+            {
+                return _isOk;
+            }
+        }
+
+        // click ok button
+        private void _okButton_Click(object sender, EventArgs e)
+        {
+            _isOk = true;
+        }
+
+        // click cancel button
+        private void _cancelButton_Click(object sender, EventArgs e)
+        {
+            _isOk = false;
+        }
+
+        // handle text box text changed
+        private void HandleTextBoxTextChanged(object sender, EventArgs e)
+        {
+            _okButton.Enabled = false;
+            bool isTransferToDouble = double.TryParse(_leftTextBox.Text, out _left) && double.TryParse(_topTextBox.Text, out _top) && double.TryParse(_rightTextBox.Text, out _right) && double.TryParse(_bottomTextBox.Text, out _bottom);
+            if (isTransferToDouble)
+            {
+                bool isInRange = IsXInRange(_left) && IsYInRange(_top) && IsXInRange(_right) && IsYInRange(_bottom);
+                if (isInRange)
+                {
+                    _okButton.Enabled = _left < _right && _top < _bottom;
+                }
+            }
+        }
+
+        // get left
+        public double GetLeft()
+        {
+            return _left;
+        }
+
+        // get top
+        public double GetTop()
+        {
+            return _top;
+        }
+
+        // get right
+        public double GetRight()
+        {
+            return _right;
+        }
+
+        // get bottom
+        public double GetBottom()
+        {
+            return _bottom;
+        }
+
+        // set size
+        public void SetCavasSize(Size currentSize)
+        {
+            _canvasSize = currentSize;
+        }
+
+        // is x in range
+        public bool IsXInRange(double pointX)
+        {
+            return (pointX >= 0 && pointX <= _canvasSize.Width);
+        }
+
+        // is y in range
+        public bool IsYInRange(double pointY)
+        {
+            return (pointY >= 0 && pointY <= _canvasSize.Height);
         }
     }
 }
